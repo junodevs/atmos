c9d86891dd99fad64a269eef1fa177790de15337
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-02 16:33:48 -0500

    More on the framework, setup my environment more

--- bot.js
+++ bot.js
@@ -1,3 +1,24 @@
-var Discord = require('discord.js')
-var fs = require('fs')
-var client = new Discord.Client()
+const Discord = require('discord.js')
+const fs = require('fs')
+const Enmap = require('enmap')
+const client = new Discord.Client()
+const package = require('./package.json');
+
+client.on('ready', () => {
+    console.log('[Connected to Discord]')
+    console.log('Startup should begin soon...')
+    client.user.setPresence({
+        game: {
+            name: `${client.guilds.size} servers | PRE ${package.version}`,
+            type: 'WATCHING'
+        },
+
+        status: 'online',
+    })
+})
+
+if (process.env.BOT_TOKEN === undefined) {
+    client.login(auth.TESTER_TOKEN);
+  } else {
+    client.login(process.env.BOT_TOKEN);
+  }
