50450a5d7a14769134905067a705ef14a9a2633e
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-03 13:26:50 -0500

    Message handler is not complete but everything else should be done. Travis will still mark this build as a fail for a couple of reasons I need to work out with luke, but we're make good progress. Should have the go ahead to start on the web dashboard soon.

---
+++ src/events/message.js
@@ -0,0 +1,9 @@
+var bot = require('../bot.js');
+const cache = require('../utils/cache.js');
+const { dbPromise } = require('../utils/database.js');
+
+module.exports = (client, mesage) => {
+    if (message.author.bot) return
+    var embed = new bot.Discord.RichEmbed()
+
+}
