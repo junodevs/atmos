3b74d8a12415f5da5c462a61b0a4f69ec14ce53d
Luke Whrit <38197656+324Luke@users.noreply.github.com>
2019-04-04 15:31:46 -0400

    Style fixes

--- src/events/message.js
+++ src/events/message.js
@@ -1,9 +1,8 @@
-var bot = require('../bot.js');
-const cache = require('../utils/cache.js');
-const { dbPromise } = require('../utils/database.js');
+var bot = require('../bot.js')
+const cache = require('../utils/cache.js')
+const { dbPromise } = require('../utils/database.js')
 
 module.exports = (client, mesage) => {
-    if (message.author.bot) return
-    var embed = new bot.Discord.RichEmbed()
-
+  if (message.author.bot) return
+  var embed = new bot.Discord.RichEmbed()
 }
