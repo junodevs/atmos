6e17da552350066faa1e92ebffb288a8cb1e1cc2
Jack <yoshifan509@gmail.com>
2019-04-04 14:07:38 -0500

    Revert "Revert "removed semicolons to maybe help fix Travis issue.""
    This reverts commit 14efa2b0fb607ac5292ddce46eff8dc00d4991c5.

--- src/events/message.js
+++ src/events/message.js
@@ -1,6 +1,6 @@
-var bot = require('../bot.js');
-const cache = require('../utils/cache.js');
-const { dbPromise } = require('../utils/database.js');
+var bot = require('../bot.js')
+const cache = require('../utils/cache.js')
+const { dbPromise } = require('../utils/database.js')
 
 module.exports = (client, mesage) => {
     if (message.author.bot) return
