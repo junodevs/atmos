93c3eaeddf32fa1c3cf56eb5c3ff0c9d7a542044
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-03 20:56:58 -0500

    gg

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Atmos
 
-[![Greenkeeper badge](https://badges.greenkeeper.io/junodevs/atmos.svg)](https://greenkeeper.io/)
+[![Greenke](https://badges.greenkeeper.io/junodevs/atmos.svg)](https://greenkeeper.io/)
 [![License](https://img.shields.io/github/license/junodevs/atmos.svg)](LICENSE)
 [![Discord](https://img.shields.io/discord/562437899707219978.svg?color=%237289DA&logo=join)](https://discord.gg/53jCeTX)
 [![Travis CI](https://api.travis-ci.org/junodevs/atmos.svg?branch=master)](https://travis-ci.org/junodevs/atmos)
@@ -8,8 +8,8 @@
 A WIP multi-purpose discord bot written in Javascript by the Juno development team for moderation, fun, and interacting with your community!
 
 * **Get Support:** Join the [discord](https://discord.gg/53jCeTX)
-* **See new features:** View our [trello](#)
-* **Invite the bot:** Invite the bot by clicking [here](#)
+* **See new features:** View our [trello](https://trello.com/b/50E1Cd6I/atmos)
+* **Invite the bot:** We're not finished yet so the bot isn't ready to be invited, we're looking to release soon though!
 
 ## Current features
 
--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,11 @@ const fs = require('fs')
 const Enmap = require('enmap')
 const mysql = require('mysql')
 const client = new Discord.Client()
-const package = require('../package.json')
-const config = require('../config.example.js/index.js')
+const path = require('path')
+const {
+  version
+} = require('../package.json')
+const config = require('../config.js')
 
 const db = mysql.createPool({
   connectionLimit: 1,
@@ -19,20 +22,23 @@ module.exports = {
   Discord: Discord,
   client: client,
   db: db,
-  reactions: config.reactions,
+  reactions: config.reactions
 }
 
 console.log('[Start Events Load]')
 
-fs.readdir('./events/', (err, files) => {
+fs.readdir('./src/events/', (err, files) => {
   if (err) return console.error(new Error(err))
   files.forEach(file => {
     if (!file.endsWith('.js')) return
-    const event = require(`./events/${file}`)
+
+    const event = require(path.join(__dirname, `/src/events/${file}`))
+
     let eventName = file.split('.')[0]
-    let embed = new Discord.RichEmbed()
+    // let embed = new Discord.RichEmbed()
+
     client.on(eventName, event.bind(null, client))
-    delete require.cache[require.resolve(`./events/${file}`)]
+    delete require.cache[require.resolve(path.join(__dirname, `/src/events/${file}`))]
   })
 })
 
@@ -42,11 +48,11 @@ client.commands = new Enmap()
 
 console.log('[Commands Load Start]')
 
-fs.readdir('./commands/', (err, files) => {
+fs.readdir('./src/commands/', (err, files) => {
   if (err) return console.error(err)
   files.forEach(file => {
     if (!file.endsWith('.js')) return
-    let props = require(`./commands/${file}`)
+    let props = require(path.join(__dirname, `/src/commands/${file}`))
     let commandName = file.split('.')[0]
     client.commands.set(commandName, props)
   })
@@ -56,19 +62,20 @@ console.log('[Commands Load Complete]')
 
 client.on('ready', () => {
   console.log('[Connected to Discord]')
-  console.log('Startup should begin soon...')
+  console.log('[Startup should begin soon...]')
   client.user.setPresence({
     game: {
-      name: `${client.guilds.size} servers | PRE ${package.version}`,
+      name: `${client.guilds.size} servers | PRE ${version}`,
       type: 'WATCHING'
     },
 
-    status: 'online',
+    status: 'online'
   })
 })
 
 if (process.env.BOT_TOKEN === undefined) {
-  client.login(auth.TESTER_TOKEN)
+  //client.login(process.env.TESTER_TOKEN)
+  client.login('NDQ3ODM4Mzg4OTQzNDU0MjA5.XKUngg.yeZ2orwDtNDt58jWvw1Zq_aWU28')
 } else {
   client.login(process.env.BOT_TOKEN)
 }
