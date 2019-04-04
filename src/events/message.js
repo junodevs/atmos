5a55d2da1a5b8d1d2c656f9d89be68b4cb0b72a6
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-04 08:27:26 -0500

    More message handling, not near done yet

--- src/events/message.js
+++ src/events/message.js
@@ -6,4 +6,20 @@ module.exports = (client, mesage) => {
     if (message.author.bot) return
     var embed = new bot.Discord.RichEmbed()
 
+    if (message.channel.type === 'dm') {
+        // TODO: ModMail features execute here
+    }
+
+    var prefix;
+    // BEGIN SHITSTORM OF CUSTOM PREFIXES
+    if (!cache.getPrefixCache(message.guild.id)) {
+        // This where we get prefix from db and put to cache
+    } else {
+        prefix = cache.getPrefixCache(message.guild.id)
+        commandRun() // REQUIRED BECAUSE MYSQL IS ASYNC
+    }
+
+    function commandRun() {
+        // HANDLE MESSAGE NOW THAT WE HAVE PREFIX
+    }
 }
