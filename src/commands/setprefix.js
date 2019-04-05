30322691237317ea5709232b6255dbad6bbb855d
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-05 10:43:17 -0500

    Suck it standard (Finished our first command: setprefix)

--- src/commands/setprefix.js
+++ src/commands/setprefix.js
@@ -1,27 +1,64 @@
 const cache = require('../utils/cache.js')
 const { dbPromise } = require('../utils/database.js')
+const bot = require('../bot')
 
 exports.run = (client, message, args, embed, thumbImg, reactions, thumbColor) => {
+  var sqlselect = `SELECT ServerID FROM config_prefix WHERE ServerID=${message.guild.id}`
+  var sqlinsert = `INSERT INTO config_prefix (ServerID, Prefix) VALUES (${message.guild.id}, '${args[0]}')`
+  var sqlupdate = `UPDATE config_prefix SET Prefix='${args[0]}' WHERE ServerID=${message.guild.id}`
 
-    var sqlselect = `SELECT ServerID FROM config_prefix WHERE ServerID=${message.guild.id}`
-    var sqlinsert = `INSERT INTO config_prefix (ServerID, Prefix) VALUES (${message.guild.id}, "${args[0]}")`
-    var sqlupdate = `UPDATE config_prefix SET Prefix="${args[0]}" WHERE ServerID=${message.guild.id}`
-
-    if (message.guild.member(message.author).hasPermission('MANAGE_GUILD')) {
-        if (args.length === 1) {
-            dbPromise(sqlselect).then((result) => {
-                if (result.length === 0) {
-                    return dbPromise(sqlinsert)
-                } else {
-                    return dbPromise(sqlupdate)
-                } // TODO: Error and success messages and reactions
-            }).then((result) => {
-                // Send success
-            })
+  if (message.guild.member(message.author).hasPermission('MANAGE_GUILD')) {
+    if (args.length === 1) {
+      dbPromise(sqlselect).then((result) => {
+        if (result.length === 0) {
+          return dbPromise(sqlinsert)
         } else {
-            // Send error because too little or too many arguments provided
+          return dbPromise(sqlupdate)
         }
+      }).then(() => {
+        cache.setPrefixCache(message.guild.id, args[0])
+        embed.setTitle('Prefix Change Successful')
+        embed.setDescription(`Prefix changed to: **'${args[0]}'**`)
+        embed.setColor(thumbColor)
+        embed.setFooter(`${message.author.username + message.author.discriminator} | ❤ JunoDevs`)
+        embed.setTimestamp(new Date())
+        embed.setThumbnail(thumbImg)
+        embed.addField(`The original prefix (**${bot.config.prefix}**) will still continue to work for running commands, alongside your custom prefix.`)
+        message.channel.send(embed)
+
+        message.react(reactions.success)
+      }).catch((err) => {
+        console.log(err)
+        embed.setTitle('Prefix Change Failed')
+        embed.setDescription('A database error occurred. Please try again later.')
+        embed.setColor(thumbColor)
+        embed.setFooter(`${message.author.username + message.author.discriminator} | ❤ JunoDevs`)
+        embed.setTimestamp(new Date())
+        embed.setThumbnail(thumbImg)
+        message.channel.send(embed)
+
+        message.react(reactions.error)
+      })
     } else {
-        // Send error because you need the MANAGE_GUILD permission to set the prefix for the server
+      embed.setTitle('Error')
+      embed.setDescription('You must provide the new prefix you wish to use without any spaces!')
+      embed.setColor(thumbColor)
+      embed.setFooter(`${message.author.username + message.author.discriminator} | ❤ JunoDevs`)
+      embed.setTimestamp(new Date())
+      embed.setThumbnail(thumbImg)
+      message.channel.send(embed)
+
+      message.react(reactions.error)
     }
+  } else {
+    embed.setTitle('Permission Error')
+    embed.setDescription('You must have the MANAGE_SERVER permission to execute this command!')
+    embed.setColor(thumbColor)
+    embed.setFooter(`${message.author.username + message.author.discriminator} | ❤ JunoDevs`)
+    embed.setTimestamp(new Date())
+    embed.setThumbnail(thumbImg)
+    message.channel.send(embed)
+
+    message.react(reactions.error)
+  }
 }
