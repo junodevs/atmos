7b9649570847e2323decf7ce79d4d214ce1386c5
Luke Whrit <38197656+324Luke@users.noreply.github.com>
2019-04-04 22:38:48 -0400

    Removed config.js from .gitignore
    REEeee

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  admins: [
+    189850839660101632,
+    200616508328509442,
+    306503388500066306,
+    401792058970603539,
+    251788826232619008
+  ],
+  prefix: 'a.',
+  reactions: {
+    success: '✅',
+    error: '❌',
+    warning: '⚠️'
+  },
+  footer: `Author#0000 | ❤ JunoDevs`,
+  thumbColor: `#686de0`
+}
