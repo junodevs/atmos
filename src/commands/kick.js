f3fe41902c774faf4a601cd0bf7cb353f6e0a501
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-06 10:05:29 -0500

    Started kick command

---
+++ src/commands/kick.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) => {
+  // ARG 1: User being banned
+  // ARG 2: Reason for kick (can use spaces, optional)
+  if (message.guild.member(message.author).hasPermission('KICK_MEMBERS')) {
+    if (args.length >= 1) {
+      
+    } else {
+      // Need arguments error msg
+    }
+  } else {
+    // Require kick members permission to kick members ;)
+  }
+}
