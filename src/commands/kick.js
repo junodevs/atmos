dd121bf140478a99012b73fdcf6cb9e2351bf90b
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-06 10:06:10 -0500

    Standard

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -3,7 +3,6 @@ exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) =
   // ARG 2: Reason for kick (can use spaces, optional)
   if (message.guild.member(message.author).hasPermission('KICK_MEMBERS')) {
     if (args.length >= 1) {
-      
     } else {
       // Need arguments error msg
     }
