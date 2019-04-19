314ae07dd2db6a600dc974545523f4af6d5311ac
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-19 11:31:56 -0500

    Holy shit it work sexcept we're still scraping it  its a long load time nothing i can do about that

--- src/commands/ow.js
+++ src/commands/ow.js
@@ -1,57 +1,104 @@
 const ow = require('overwatch-api')
 exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) => {
+  message.channel.startTyping()
   if (args.length === 3) {
     var playerName = args[0].replace('#', '-')
 
     var region
     var platform
-    if (args[1] === 'us') region = 'us'
-    if (args[1] === 'eu') region = 'eu'
+    if (args[1] === 'us') {
+      region = 'us'
+    } else if (args[1] === 'eu') {
+      region = 'eu'
+    } else {
+      embed.setTitle('Region Error')
+      embed.setDescription('The provided region was invalid.')
+      embed.setColor(embedColors.error)
+      embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
+      embed.setTimestamp(new Date())
+      embed.setThumbnail(thumbImg)
+      embed.addField('Command Syntax', 'For command syntax do the command without arguments.')
 
-    if (args[2] === 'pc') platform = 'pc'
-    if (args[2] === 'xbl') platform = 'xbl'
-    if (args[2] === 'psn') platform = 'psn'
+      message.channel.send(embed)
+      message.react(reactions.error)
+      message.channel.stopTyping()
+      return
+    }
+
+    if (args[2] === 'pc') {
+      platform = 'pc'
+    } else if (args[2] === 'xbl') {
+      platform = 'xbl'
+    } else if (args[2] === 'psn') {
+      platform = 'psn'
+    } else {
+      embed.setTitle('Platform Error')
+      embed.setDescription('The provided platform was invalid.')
+      embed.setColor(embedColors.error)
+      embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
+      embed.setTimestamp(new Date())
+      embed.setThumbnail(thumbImg)
+      embed.addField('Command Syntax', 'For command syntax do the command without arguments.')
+
+      message.channel.send(embed)
+      message.react(reactions.error)
+      message.channel.stopTyping()
+      return
+    }
 
     ow.getStats(platform, region, playerName, (err, json) => {
       if (err) {
-        console.log(err)
         embed.setTitle('Retrieval Error')
-        embed.setDescription('There was an error finding the specified username in the specified platform and region. This is probably an issue with blizzard. Please try again later.')
+        embed.setDescription('There was an error finding the specified username in the specified platform and region. This is probably an issue with blizzard, but it could also be a typo.')
         embed.setColor(embedColors.error)
         embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
         embed.setTimestamp(new Date())
         embed.setThumbnail(thumbImg)
+        embed.addField('Command Syntax', 'For command syntax do the command without arguments.')
 
         message.channel.send(embed)
         message.react(reactions.error)
+        message.channel.stopTyping()
         return
       }
 
       if (json.private) {
         console.log(err)
         embed.setTitle('Privacy Error')
-        embed.setDescription('**Your profile is set to private so Atmos cannot retrieve any stats or information on your gameplay. This can be modified by the user in-game.**')
+        embed.setDescription('**Your profile is set to private, so Atmos cannot retrieve any stats or information on your gameplay. This can be modified by the user in-game.**')
         embed.setColor(embedColors.error)
         embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
         embed.setTimestamp(new Date())
         embed.setThumbnail(thumbImg)
+        embed.addField('Command Syntax', 'For command syntax do the command without arguments.')
 
         message.channel.send(embed)
         message.react(reactions.error)
+        message.channel.stopTyping()
         return
       }
 
-      console.log(json)
-      embed.setTitle(`Overwatch Player Stats: ${json.username}`)
-      embed.setDescription('A summary of statistics for the specified overwatch profile.')
-      embed.setColor(embedColors.success)
-      embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
-      embed.setTimestamp(new Date())
-      embed.setThumbnail(json.portrait)
-      embed.addField(`Level: ${json.level}`, 'More info to come soon!')
+      ow.getProfile(platform, region, playerName, (err, profile) => {
+        if (err) console.log(err)
 
-      message.channel.send(embed)
-      message.react(reactions.success)
+        embed.setTitle(`Overwatch Player Stats: ${json.username}`)
+        embed.setDescription(`Level: ${json.level}`)
+        embed.setColor(embedColors.success)
+        embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
+        embed.setTimestamp(new Date())
+        embed.setThumbnail(json.portrait)
+        embed.setAuthor(`Competitive Rank: ${profile.competitive.rank}`, profile.competitive.rank_img)
+        embed.addField('Endorsement Level:', json.endorsement.level, true)
+        embed.addField('Comp Games Won:', json.stats.game.competitive[2].value, true)
+        embed.addField('Comp Games Lost:', json.stats.game.competitive[0].value, true)
+        embed.addField('Comp Time Played:', json.stats.game.competitive[3].value, true)
+        embed.addField('Quickplay Games Won:', json.stats.game.quickplay[0].value, true)
+        embed.addField('Quickplay Time Played:', json.stats.game.quickplay[1].value, true)
+
+        message.channel.send(embed)
+        message.react(reactions.success)
+        message.channel.stopTyping()
+      })
     })
   } else {
     embed.setTitle('Command Error')
@@ -66,5 +113,6 @@ exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) =
 
     message.channel.send(embed)
     message.react(reactions.error)
+    message.channel.stopTyping()
   }
 }
