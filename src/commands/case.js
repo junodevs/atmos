05ea68e3ad7a53291921619182ca7a416f61deba
Max Bromberg <maxwell.bromberg@gmail.com>
2019-04-19 22:07:47 -0500

    More on the case command

--- src/commands/case.js
+++ src/commands/case.js
@@ -8,11 +8,11 @@ exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) =
     if (args[0].toLowerCase() === 'ban') {
       selectsql = `SELECT CaseID,ServerID,UserID,BanReason,Date FROM bans WHERE CaseID=${args[1]}`
     } else if (args[0].toLowerCase() === 'kick') {
-      selectsql = `SELECT CaseID FROM kicks WHERE CaseID=${args[1]}`
+      selectsql = `SELECT CaseID,ServerID,UserID,KickReason,Date FROM kicks WHERE CaseID=${args[1]}`
     } else if (args[0].toLowerCase() === 'mute') {
-      selectsql = `SELECT CaseID FROM mutes WHERE CaseID=${args[1]}`
+      selectsql = `SELECT CaseID,ServerID,UserID,MuteReason,Date FROM mutes WHERE CaseID=${args[1]}`
     } else if (args[0].toLowerCase() === 'warning') {
-      selectsql = `SELECT CaseID FROM warnings WHERE CaseID=${args[1]}`
+      selectsql = `SELECT CaseID,ServerID,UserID,WarnReason,Date FROM warnings WHERE CaseID=${args[1]}`
     } else {
       // Not a valid punish type
       return
@@ -21,14 +21,29 @@ exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) =
     dbPromise(selectsql).then(result => {
       if (result === [] || result[0] === undefined) {
         // No case found
+        console.log('No Case Found')
       } else {
-        if (result.ServerID === message.guild.id) {
-          embed.setTitle(`Case Info: ${args[1]}`)
+        var caseinfo = result[0]
+        var reason
+        if (args[0].toLowerCase() === 'ban') {
+          reason = caseinfo.BanReason
+        } else if (args[0].toLowerCase() === 'kick') {
+          reason = caseinfo.KickReason
+        } else if (args[0].toLowerCase() === 'mute') {
+          reason = caseinfo.MuteReason
+        } else if (args[0].toLowerCase() === 'warning') {
+          reason = caseinfo.WarnReason
+        }
+        
+        if (caseinfo.ServerID = message.guild.id) {
+          embed.setTitle(`Case Info: ${caseinfo.CaseID}`)
           embed.setColor(embedColors.success)
           embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
           embed.setTimestamp(new Date())
           embed.setThumbnail(thumbImg)
-          embed.addField()
+          embed.addField('User Punished', `<@${caseinfo.UserID}>`)
+          embed.addField('Punishment Reason', reason)
+          embed.addField('Date Punished', caseinfo.Date)
 
           message.channel.send(embed)
           message.react(reactions.success)
