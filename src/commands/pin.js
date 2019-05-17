504c2a79010a016befdcbc57b3409406dcda7e93
Jack <yoshifan509@gmail.com>
2019-05-17 13:55:45 +0000

    Fix standard in pin.js

--- src/commands/pin.js
+++ src/commands/pin.js
@@ -1,15 +1,15 @@
 exports.run = (client, message, args, embed, thumbImg, reactions, embedColors) => {
   var msg = message.channel.fetchMessage(args[0])
   if (message.guild.channels.exists('name', 'pinboard')) {
-      var chnl = message.guild.channels.get('name', 'pinboard')
+    var chnl = message.guild.channels.get('name', 'pinboard')
   } else {
-      message.guild.createChannel('pinboard', "text")
-      embed.setTitle('Created Pinboard channel.')
-      embed.setColor(embedColors.default)
-      embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
-      embed.setTimestamp(new Date())
-      embed.setThumbnail(thumbImg)
-      message.channel.send(embed)
+    message.guild.createChannel('pinboard', 'text')
+    embed.setTitle('Created Pinboard channel.')
+    embed.setColor(embedColors.default)
+    embed.setFooter(`${message.author.username + '#' + message.author.discriminator} | ❤ JunoDevs`)
+    embed.setTimestamp(new Date())
+    embed.setThumbnail(thumbImg)
+    message.channel.send(embed)
   }
   embed.setTitle(':pushpin: Pinned:')
   embed.setColor(embedColors.default)
