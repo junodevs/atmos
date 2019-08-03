971290a2cfb6b27bf4cd346745c0a11950526c2c
Luke <38197656+324Luke@users.noreply.github.com>
2019-08-03 17:09:15 -0400

    add support for .mjs files to akairo

--- src/bot.js
+++ src/bot.js
@@ -9,53 +9,8 @@ require('dotenv').config()
 
 const client = new AkairoClient({
   ownerID: config.admins,
-  prefix: message => 'a.',
-  // #region
-  // prefix: message => {
-  //   var prefix // Perfectly balanced, as all things should be.
-  //   if (!cache.getPrefixCache(message.guild.id)) {
-  //     const sqlCheck = `SELECT Prefix FROM config WHERE ServerID=${db.escape(parseInt(message.guild.id))}`
-  //     dbPromise(sqlCheck).then((result) => {
-  //       if (result[0] === undefined) {
-  //         cache.setPrefixCache(message.guild.id, config.prefix)
-  //         prefix = config.prefix // Very slight performance boost vs last version
-  //         resultMePlease()
-  //       } else {
-  //         var prefixResult = result[0]['Prefix']
-  //         cache.setPrefixCache(message.guild.id, prefixResult)
-  //         prefix = prefixResult // Very slight performance boost again
-  //         resultMePlease()
-  //       }
-  //     }).catch((err) => {
-  //       console.log(err)
-
-  //       this.client.user.setPresence({
-  //         game: {
-  //           name: `DB Error! | We're already on it!`
-  //         },
-  //         status: 'dnd'
-  //       })
-  //       var embed = new Discord.RichEmbed()
-  //       embed.setTitle('Command Run Error')
-  //       embed.setDescription('There was an error in the command run process.')
-  //       embed.setColor(config.colors.error)
-  //       embed.setFooter(`Chief we've got a problem`)
-  //       embed.setTimestamp(new Date())
-  //       embed.setThumbnail(config.thumbImg)
-  //       // ERROR: Notifies Devs for fixing
-  //       embed.addField('`' + err + '`', 'Above is the error message. This message is the result of a problem anywhere from a DB connection error to an unhandled promise in a command file. This is an urgent, error, and must be handled as soon as possible by the team.')
-
-  //       this.client.guilds.get('561768427757240330').channels.get('563495099896430612').send('<@&563495674432192513>', { embed: embed })
-  //     })
-  //   } else {
-  //     prefix = cache.getPrefixCache(message.guild.id)
-  //     resultMePlease()
-  //   }
-  //   function resultMePlease () {
-  //     return prefix
-  //   }
-  // },
-  // #endregion
+  prefix: 'a.',
+  extensions: [ '.js', '.ts', '.mjs' ],
   clientUtil: true,
   handleEdits: true,
   defaultCooldown: 2000,
