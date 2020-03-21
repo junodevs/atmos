abb5c7f657e7f9e70210683c34463dd024d71872
BluLightShow <maxwell.bromberg@gmail.com>
2020-03-20 23:00:22 -0500

    Ok thank you discord.js v12

--- src/events/message.js
+++ src/events/message.js
@@ -5,7 +5,7 @@ const config = require('../../config')
 
 module.exports = (client, message) => {
   if (message.author.bot) return
-  const embed = new bot.Discord.RichEmbed()
+  const embed = new bot.Discord.MessageEmbed()
 
   if (message.channel.type === 'dm') {
     // TODO: MODMAIL
